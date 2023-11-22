a06c74d4426e1f1d3f434b08a22bf0d5e16abc74
Manish <my79220@gmail.com>
2023-11-22 20:49:06 +0530

    added

--- src/app/Components/Cards.jsx
+++ src/app/Components/Cards.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Cards = () => {
   return (
     <div>
-
       <div class="px-3 mt-10 mb-10 ">
         <div class=" text-gray-600 ">
           <div class="mb-12 space-y-2 text-center">
-            <h2 class="text-3xl font-bold text-gray-700 md:text-4xl dark:text-white">Services Offered</h2>
-            
+            <h2 class="text-3xl font-bold text-gray-700 md:text-4xl dark:text-white">
+              Services Offered
+            </h2>
           </div>
           <section class="">
             <div class="flex justify-center flex-wrap ">
-
               <a href="">
                 <div class="flex w-full">
                   <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                    <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://wickedblocks.dev/assets/images/placeholders/neon-1.jpg" alt="blog" />
+                    <img
+                      class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
+                      src="https://wickedblocks.dev/assets/images/placeholders/neon-1.jpg"
+                      alt="blog"
+                    />
                     <div class="px-6 py-8">
-                      <h4 class="mt-4 text-xl font-semibold text-neutral-600">
-                       <span className='text-4xl relative  -top-[5px] font-extrabold text-blue-500'>.</span><span class="ml-2">Presentations</span>
-                      </h4>
-                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                      <button className='rounded-full mt-6 w-8 h-8 bg-gray-800'>
-                      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                      <div className="flex align-center">
+                        <div className="bg-blue-500 relative top-2 rounded-full w-2 h-2"></div>
+                        <h4 class="ml-4  text-xl font-semibold text-neutral-600">
+                          Presentations
+                        </h4>
+                      </div>
+
+                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">
+                        Install Tailwind CSS without any Javascript Framewrok
+                        locally with purgeCSS, enable the dark mode option,
+                        prefferences or class is upt to you.
+                      </p>
+                      <button className="rounded-full mt-6 w-8 h-8 bg-gray-800">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M6 12H18M18 12L13 7M18 12L13 17"
+                              stroke="#ffffff"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>{" "}
+                          </g>
+                        </svg>
                       </button>
                     </div>
                   </div>
@@ -32,14 +64,46 @@ const Cards = () => {
               <a href="">
                 <div class="flex w-full">
                   <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                    <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://wickedblocks.dev/assets/images/placeholders/neon-1.jpg" alt="blog" />
+                    <img
+                      class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
+                      src="https://wickedblocks.dev/assets/images/placeholders/neon-1.jpg"
+                      alt="blog"
+                    />
                     <div class="px-6 py-8">
-                      <h4 class="mt-4 text-xl font-semibold text-neutral-600">
-                      <span className='text-4xl relative  -top-[5px] font-extrabold text-blue-500'>.</span><span class="ml-2">WebApp Development</span>
-                      </h4>
-                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                      <button className='rounded-full mt-6 w-8 h-8 bg-gray-800'>
-                      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                      <div className="flex align-center">
+                        <div className="bg-blue-500 relative top-2 rounded-full w-2 h-2"></div>
+                        <h4 class="ml-4  text-xl font-semibold text-neutral-600">
+                          WebApp Development
+                        </h4>
+                      </div>
+                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">
+                        Install Tailwind CSS without any Javascript Framewrok
+                        locally with purgeCSS, enable the dark mode option,
+                        prefferences or class is upt to you.
+                      </p>
+                      <button className="rounded-full mt-6 w-8 h-8 bg-gray-800">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M6 12H18M18 12L13 7M18 12L13 17"
+                              stroke="#ffffff"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>{" "}
+                          </g>
+                        </svg>
                       </button>
                     </div>
                   </div>
@@ -49,44 +113,106 @@ const Cards = () => {
               <a href="">
                 <div class="flex w-full">
                   <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                    <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://wickedblocks.dev/assets/images/placeholders/neon-4.jpg" alt="blog" />
+                    <img
+                      class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
+                      src="https://wickedblocks.dev/assets/images/placeholders/neon-4.jpg"
+                      alt="blog"
+                    />
                     <div class="px-6 py-8">
-                      <h4 class="mt-4 text-xl font-semibold text-neutral-600">
-                      <span className='text-4xl relative  -top-[5px] font-extrabold text-blue-500'>.</span><span class="ml-2">UI/UX Design</span>
-                      </h4>
-                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                      <button className='rounded-full mt-6 w-8 h-8 bg-gray-800'>
-                      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                      <div className="flex align-center">
+                        <div className="bg-blue-500 relative top-2 rounded-full w-2 h-2"></div>
+                        <h4 class="ml-4  text-xl font-semibold text-neutral-600">
+                          UI/UX Design
+                        </h4>
+                      </div>
+                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">
+                        Install Tailwind CSS without any Javascript Framewrok
+                        locally with purgeCSS, enable the dark mode option,
+                        prefferences or class is upt to you.
+                      </p>
+                      <button className="rounded-full mt-6 w-8 h-8 bg-gray-800">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M6 12H18M18 12L13 7M18 12L13 17"
+                              stroke="#ffffff"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>{" "}
+                          </g>
+                        </svg>
                       </button>
                     </div>
                   </div>
                 </div>
               </a>
 
-              <a href="">
-                <div class="flex w-full">
+              <div >
+                <div class="flex cursor-pointer group w-full">
                   <div class="relative flex flex-col items-start m-1 transition duration-300 ease-in-out delay-150 transform bg-white shadow-2xl rounded-xl md:w-80 md:-ml-16 md:hover:-translate-x-16 md:hover:-translate-y-8">
-                    <img class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36" src="https://wickedblocks.dev/assets/images/placeholders/neon-2.jpg" alt="blog" />
+                    <img
+                      class="object-cover object-center w-full rounded-t-xl lg:h-48 md:h-36"
+                      src="https://wickedblocks.dev/assets/images/placeholders/neon-2.jpg"
+                      alt="blog"
+                    />
                     <div class="px-6 py-8">
-                      <h4 class="mt-4 text-xl font-semibold text-neutral-600">
-                      <span className='text-4xl relative  -top-[5px] font-extrabold text-blue-500'>.</span><span class="ml-2">College Projects</span>
-                      </h4>
-                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">Install Tailwind CSS without any Javascript Framewrok locally with purgeCSS, enable the dark mode option, prefferences or class is upt to you.</p>
-                      <button className='rounded-full mt-6 w-8 h-8 bg-gray-800'>
-                      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                      <div className="flex align-center">
+                        <div className="bg-blue-500 relative top-2 rounded-full w-2 h-2"></div>
+                        <h4 class="ml-4  text-xl font-semibold text-neutral-600">
+                          College Projects
+                        </h4>
+                      </div>
+                      <p class="mt-4 text-sm font-normal text-gray-500 leading-relax">
+                        Install Tailwind CSS without any Javascript Framewrok
+                        locally with purgeCSS, enable the dark mode option,
+                        prefferences or class is upt to you.
+                      </p>
+                      <button className="rounded-full mt-6 w-8 h-8     bg-gray-800">
+                        <svg
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                          <g
+                            id="SVGRepo_tracerCarrier"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></g>
+                          <g id="SVGRepo_iconCarrier">
+                            {" "}
+                            <path
+                              d="M6 12H18M18 12L13 7M18 12L13 17"
+                              stroke="#ffffff"
+                              stroke-width="2"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>{" "}
+                          </g>
+                        </svg>
                       </button>
                     </div>
                   </div>
                 </div>
-              </a>
-
+              </div>
             </div>
           </section>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
