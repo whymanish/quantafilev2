3f41511b63cb3f36563294808a4791e842d02484
Manish <my79220@gmail.com>
2023-11-23 19:14:26 +0530

    added

--- src/app/Components/FTP.jsx
+++ src/app/Components/FTP.jsx
@@ -4,7 +4,7 @@ const VerticalCarousel = () => {
     return (
         <div>
             <section>
-                <div class="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
+                <div class="relative items-center w-full px-5 pb-20 mx-auto md:px-12 lg:px-16 max-w-7xl">
                     <div class="relative flex-col items-start m-auto align-middle">
                     <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
                             <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
@@ -18,7 +18,45 @@ const VerticalCarousel = () => {
                                 <div class="max-w-xl text-center lg:text-left">
                                     <div>
                                         <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
-                                            UI/UX DEVELOPMENT
+                                            PRESENTATION
+                                        </p>
+                                        <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
+                                            Use this paragraph to share information about your company
+                                            or products. Make it engaging and interesting, and
+                                            showcase your brand's personality. Thanks for visiting our
+                                            website!
+                                        </p>
+                                    </div>
+                                    <div class="flex flex-col items-center justify-center gap-3 mt-10 lg:flex-row lg:justify-start">
+                                        <a
+                                            href="#_"
+                                            class="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none lg:w-auto focus-visible:outline-black text-sm focus-visible:ring-black"
+                                        >
+                                            Button
+                                        </a>
+                                        <a
+                                            href="#_"
+                                            class="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600"
+                                        >
+                                            Learn more &nbsp; →
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
+                                <img
+                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
+                                    alt="hero"
+                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                />
+                            </div>
+                            <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
+                                <div class="max-w-xl text-center lg:text-left">
+                                    <div>
+                                        <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
+                                           WEB DEVELOPMENT
                                         </p>
                                         <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
                                             Use this paragraph to share information about your company
@@ -94,45 +132,7 @@ const VerticalCarousel = () => {
                                 <div class="max-w-xl text-center lg:text-left">
                                     <div>
                                         <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
-                                            UI/UX DEVELOPMENT
-                                        </p>
-                                        <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
-                                            Use this paragraph to share information about your company
-                                            or products. Make it engaging and interesting, and
-                                            showcase your brand's personality. Thanks for visiting our
-                                            website!
-                                        </p>
-                                    </div>
-                                    <div class="flex flex-col items-center justify-center gap-3 mt-10 lg:flex-row lg:justify-start">
-                                        <a
-                                            href="#_"
-                                            class="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none lg:w-auto focus-visible:outline-black text-sm focus-visible:ring-black"
-                                        >
-                                            Button
-                                        </a>
-                                        <a
-                                            href="#_"
-                                            class="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600"
-                                        >
-                                            Learn more &nbsp; →
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
-                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
-                                <img
-                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
-                                    alt="hero"
-                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                />
-                            </div>
-                            <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
-                                <div class="max-w-xl text-center lg:text-left">
-                                    <div>
-                                        <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
-                                            UI/UX DEVELOPMENT
+                                            COLLEGE PROJECTS
                                         </p>
                                         <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
                                             Use this paragraph to share information about your company
