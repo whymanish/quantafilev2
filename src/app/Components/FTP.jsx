808254e835e25bd161c8aea8995b6bd72b12b136
Manish <my79220@gmail.com>
2023-11-23 18:15:23 +0530

    added

--- src/app/Components/FTP.jsx
+++ src/app/Components/FTP.jsx
@@ -6,11 +6,18 @@ const VerticalCarousel = () => {
             <section>
                 <div class="relative items-center w-full px-5 py-24 mx-auto md:px-12 lg:px-16 max-w-7xl">
                     <div class="relative flex-col items-start m-auto align-middle">
-                        <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+                    <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
+                                <img
+                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
+                                    alt="hero"
+                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                />
+                            </div>
                             <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
                                 <div class="max-w-xl text-center lg:text-left">
                                     <div>
-                                        <p class="text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
+                                        <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
                                             UI/UX DEVELOPMENT
                                         </p>
                                         <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
@@ -35,13 +42,6 @@ const VerticalCarousel = () => {
                                         </a>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
-                                <img
-                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
-                                    alt="hero"
-                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                />
                             </div>
                         </div>
                         <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
@@ -83,10 +83,17 @@ const VerticalCarousel = () => {
                             </div>
                         </div>
                         <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
+                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
+                                <img
+                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
+                                    alt="hero"
+                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                />
+                            </div>
                             <div class="relative items-center gap-12 m-auto lg:inline-flex md:order-first">
                                 <div class="max-w-xl text-center lg:text-left">
                                     <div>
-                                        <p class="text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
+                                        <p class=" text-4xl lg:text-5xl font-extrabold tracking-tight text-black ">
                                             UI/UX DEVELOPMENT
                                         </p>
                                         <p class="max-w-xl mt-4 text-base tracking-tight text-gray-600">
@@ -111,13 +118,6 @@ const VerticalCarousel = () => {
                                         </a>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="order-first block w-full mt-12 aspect-square lg:mt-0">
-                                <img
-                                    class="object-cover object-center h-full rounded-2xl w-full mx-auto bg-gray-300 border lg:ml-auto"
-                                    alt="hero"
-                                    src="https://images.unsplash.com/photo-1590402494682-cd3fb53b1f70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                />
                             </div>
                         </div>
                         <div class="grid mt-20 grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-24">
